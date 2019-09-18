318387379e7c8ad57a5b7f2ae70ee95e9f34e6a3
Michel3951 <michel39511@gmail.com>
2019-09-18 19:16:04 +0200

    cms af

---
+++ app/Models/Page.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    /**
+     * Tell laravel which table to use
+     * @var string
+     */
+    protected $table = 'roles';
+
+    /**
+     * Laravel will be able to edit data in these columns
+     * @var array
+     */
+    protected $fillable = ['name'];
+
+    /**
+     * Get all users that have this role
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+
+    public function users()
+    {
+        return $this->hasManyThrough(User::class, UserRole::class, 'role_id', 'user_id', 'id')->get();
+    }
+}
