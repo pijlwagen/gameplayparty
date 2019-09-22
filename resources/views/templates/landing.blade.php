6ca4803f0598bab0d5b576abd3361f222403877e
nightcodex <jeroenlieverse@hotmail.com>
2019-09-22 14:11:10 +0200

    Landing page added WiP

--- resources/views/templates/landing.blade.php
+++ resources/views/templates/landing.blade.php
@@ -1,19 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-    <img src="
-https://dummyimage.com/2560x300/000600/fff" class="rounded mx-auto d-block img-fluid" alt="...">
-    <h1> Wie zijn wij? </h1>
+<div class="container">
+ <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://images.vrt.be/width960/2017/12/27/c811e6cc-eac6-11e7-8ba7-02b7b76bf47f.jpg" alt="KinePolis">
+      <button type="button" class="btn btn-info">Informatie</button>
+      <button type="button" class="btn btn-success">Reserveren</button>
+      
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 img-fluid" src="https://www.dp6.nl/uploads/63/04/84e3eaf49a52fe6f198c383f6bdf4c41ae870b274659a696f1fe5f99688d6e16-1920x_.jpg" alt="CineMac">
+      <button type="button" class="btn btn-info">Informatie</button>
+      <button type="button" class="btn btn-success">Reserveren</button>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://upload.wikimedia.org/wikipedia/commons/0/09/Wolffgroningen.jpg" alt="Wolf bioscoop">
+      <button type="button" class="btn btn-info">Informatie</button>
+      <button type="button" class="btn btn-success">Reserveren</button>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
     <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
     <br>
     <br>
     <h4> Waarom gameplayparty? </h4>
     <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.<p>
-    <img src="
-https://dummyimage.com/800x600/000600/fff" class="rounded mx-auto d-block" alt="...">
-    <br>
-    <img src=""> 
+
     <br>
     <h4> Wat bieden wij? </h4>
     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.<p>
@@ -21,6 +48,4 @@ https://dummyimage.com/800x600/000600/fff" class="rounded mx-auto d-block" alt="
     <img src=""> 
     <br>
     </div>
-
 @stop
-
