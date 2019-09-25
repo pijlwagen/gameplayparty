ee8ef254e9a64d69b5470f0671a0db931b5c1043
Michel3951 <tfhdgnetwork1@gmail.com>
2019-09-25 09:58:08 +0200

    home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,5 +12,55 @@
     </div>
     <div class="container">
         {!! $page->render() !!}
+        <div class="row my-3">
+            <div class="col-md-4">
+                <div class="card ad shadow-lg">
+                    <div class="card-body text-center">
+                        <h1>1 uur</h1>
+                        <p>Kleine omschrijving over wat dit tijdslot inhoud. Exclusief fietsenstalling. Dit komt 
+                            voor volgende sprint maar is nu opvulling.</p>
+                        <ul class="list-unstyled">
+                            <li><i class="fa fa-check text-primary"></i> Optie 1</li>
+                            <li><i class="fa fa-check text-primary"></i> Optie 2</li>
+                            <li><i class="fa fa-times text-danger"></i> Optie 3</li>
+                            <li><i class="fa fa-times text-danger"></i> Optie 4</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary">Bekijk beschikbare bioscopen</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card ad shadow-lg">
+                    <div class="card-body text-center">
+                        <h1>2 uur</h1>
+                        <p>Kleine omschrijving over wat dit tijdslot inhoud. Exclusief fietsenstalling. Dit komt 
+                            voor volgende sprint maar is nu opvulling.</p>
+                        <ul class="list-unstyled">
+                            <li><i class="fa fa-check text-primary"></i> Optie 1</li>
+                            <li><i class="fa fa-check text-primary"></i> Optie 2</li>
+                            <li><i class="fa fa-check text-primary"></i> Optie 3</li>
+                            <li><i class="fa fa-times text-danger"></i> Optie 4</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary">Bekijk beschikbare bioscopen</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card ad shadow-lg">
+                    <div class="card-body text-center">
+                        <h1>3 uur</h1>
+                        <p>Kleine omschrijving over wat dit tijdslot inhoud. Exclusief fietsenstalling. Dit komt 
+                            voor volgende sprint maar is nu opvulling.</p>
+                        <ul class="list-unstyled">
+                            <li><i class="fa fa-check text-primary"></i> Optie 1</li>
+                            <li><i class="fa fa-check text-primary"></i> Optie 2</li>
+                            <li><i class="fa fa-check text-primary"></i> Optie 3</li>
+                            <li><i class="fa fa-check text-primary"></i> Optie 4</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary">Bekijk beschikbare bioscopen</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @stop
